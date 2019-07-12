7cecbb91945369738242da0b64b435369c88939b
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2019-07-13 01:05:13 +0530

    Update 12-07-2019

---
+++ Finasty003/Assets/_Scripts/tutorial.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    public GameObject b;
+    public GameObject c;
+    public GameObject d;
+    public GameObject e;
+    // Start is called before the first frame update
+    void Start()
+    {
+        b.SetActive(false);
+        c.SetActive(false);
+        d.SetActive(false);
+        e.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
